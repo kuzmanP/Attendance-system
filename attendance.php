34c95f2d1bf750e0b8c20b2dcae11af1917cd8f0
Kuzman <georgeghartey2@gmail.com>
2023-01-02 13:15:21 +0000

    changed The Title(frontend)

--- attendance.php
+++ attendance.php
@@ -20,7 +20,7 @@ session_start();
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Attendance CheckIn Page</title>
+    <title>Attendance Check-In Page</title>
     <link rel="stylesheet" href="css/index.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.0/css/all.min.css" integrity="sha512-10/jx2EXwxxWqCLX/hHth/vu2KY3jCF70dCQB8TSgNjbCVAC/8vai53GfMDrO2Emgwccf2pJqxct9ehpzG+MTw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
@@ -31,7 +31,7 @@ session_start();
 
 
     <nav class="navbar navbar-expand-sm navbar-light bg-light">
-        <a class="navbar-brand" >
+        <a class="navbar-brand">
             <h5>&nbsp; <img class="img-fluid" src="UMaT-logo-238x300.jpg" alt="" height="40px" width="40px">&nbsp; Peace Royal Attendance System </h5>
 
         </a>
